1ab47b1ab267432ad14b299877f42978a5852fd6
minho <radlohead@naver.com>
2019-04-12 14:39:13 +0900

    command pattern is typescript

---
+++ commandPattern.ts
@@ -0,0 +1,121 @@
+type messageType = {
+    msg: string
+}
+
+const message = (msg: messageType['msg']) => {
+    console.log(`message: ${msg}`)
+}
+
+interface ILight {
+    context: string
+}
+
+class Light implements ILight {
+    context: ILight['context']
+    constructor(props: ILight['context']) {
+        this.context = props
+    }
+    public on() {
+        message(`${this.context} on`)
+    }
+    public off() {
+        message(`${this.context} off`)
+    }
+}
+
+class RoomOnCommand extends Light {
+    constructor(props?: ILight['context']) {
+        super(props)
+        this.context = 'Room'
+    }
+
+    private execute() {
+        this.on()
+    }
+    private undo() {
+        this.off()
+    }
+}
+
+class RoomOffCommand extends Light {
+    constructor(props?: ILight['context']) {
+        super(props)
+        this.context = 'Room'
+    }
+
+    private execute() {
+        this.on()
+    }
+    private undo() {
+        this.off()
+    }
+}
+
+class MusicOnCommand extends Light {
+    constructor(props?: ILight['context']) {
+        super(props)
+        this.context = 'Music'
+    }
+    private execute() {
+        this.on()
+    }
+    private undo() {
+        this.off()
+    }
+}
+
+class MusicOffCommand extends Light {
+    constructor(props?: ILight['context']) {
+        super(props)
+        this.context = 'Music'
+    }
+    private execute() {
+        this.on()
+    }
+    private undo() {
+        this.off()
+    }
+}
+
+interface IRemoteControl {
+    execute(): void
+    undo(): void
+}
+
+class RemoteControl {
+    obj: IRemoteControl
+    undo: IRemoteControl
+
+    public setCommand(obj) {
+        this.obj = obj
+    }
+    public on() {
+        this.obj.execute()
+        this.undo = this.obj
+    }
+    public off() {
+        this.obj.undo()
+    }
+}
+
+const roomOn = new RoomOnCommand()
+const roomOff = new RoomOffCommand()
+const musicOn = new MusicOnCommand()
+const musicOff = new MusicOffCommand()
+const remoteControl = new RemoteControl()
+
+remoteControl.setCommand(roomOn)
+remoteControl.on()
+remoteControl.off()
+
+remoteControl.setCommand(roomOff)
+remoteControl.on()
+remoteControl.off()
+
+remoteControl.setCommand(musicOn)
+remoteControl.on()
+remoteControl.off()
+
+remoteControl.setCommand(musicOff)
+remoteControl.on()
+remoteControl.off()
