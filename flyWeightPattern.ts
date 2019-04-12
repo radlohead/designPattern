1863e5a166296aaef23a1391daa9985d981c1e1a
minho <radlohead@naver.com>
2019-04-12 15:00:30 +0900

    flyWeight pattern

---
+++ flyWeightPattern.ts
@@ -0,0 +1,28 @@
+interface ILegionary {
+    name: string
+    hp: number
+    att: number
+}
+
+class Legionary implements ILegionary {
+    name: string
+    hp: number
+    att: number
+    constructor(name) {
+        this.name = name
+        this.hp = 50
+        this.att = 5
+    }
+
+    public attack(target) {
+        console.log(this.name + '가 ' + target + '를 공격합니다')
+    }
+}
+
+const soldier1 = new Legionary('Marcus')
+const soldier2 = new Legionary('Tiberius')
+const soldier3 = new Legionary('Zero')
+
+soldier1.hp = 40
+soldier1.hp // 40
+soldier2.hp // 50
