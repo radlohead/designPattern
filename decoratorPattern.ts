3c444aaec316d71e020e8295dfbb3b25a126fb49
minho <radlohead@naver.com>
2019-04-13 14:17:16 +0900

    decorator pattern modify typescript

--- decoratorPattern.ts
+++ decoratorPattern.ts
@@ -1,16 +1,23 @@
-class Espresso {
+interface IEspress {
+    cost: number
+    milk?: number
+    water?: number
+}
+
+class Espresso implements IEspress {
+    cost: number
     constructor() {
         this.cost = 2500
     }
 }
 
-const milk = coffee => {
+const milk = (coffee: IEspress) => {
     coffee.cost += 300
     coffee.milk = 500
     return coffee
 }
 
-const water = coffee => {
+const water = (coffee: IEspress) => {
     coffee.cost += 100
     coffee.water = 300
     return coffee
