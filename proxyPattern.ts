d76b67e190a2a3bcc714e38caf0d194744ca9e2b
minho <radlohead@naver.com>
2019-04-30 12:39:20 +0900

    proxy pattern js -> ts

--- proxyPattern.ts
+++ proxyPattern.ts
@@ -4,8 +4,13 @@ class Car {
     }
 }
 
-class CarPattern {
-    constructor(driver) {
+interface ICarPattern {
+    driver: Driver
+}
+
+class CarPattern implements ICarPattern {
+    driver: ICarPattern['driver']
+    constructor(driver: ICarPattern['driver']) {
         this.driver = driver
     }
 
@@ -15,12 +20,13 @@ class CarPattern {
 }
 
 class Driver {
-    constructor(age) {
+    age: number
+    constructor(age: number) {
         this.age = age
     }
 }
 
 const carPatternYoung = new CarPattern(new Driver(10))
-const carPatternOld = new CarPattern(new Driver(10))
+const carPatternOld = new CarPattern(new Driver(20))
 carPatternYoung.drive()
 carPatternOld.drive()
